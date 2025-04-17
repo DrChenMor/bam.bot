339f0c7d32990072408c0a0b103221e724237b76
DrChenMor <thechenmor@gmail.com>
2025-04-17 22:04:57 +0800

    Update bamba_checker.py

--- bamba_checker.py
+++ bamba_checker.py
@@ -5,10 +5,6 @@ from datetime import datetime
 # 1) List your stores here
 STORES = [
     {
-      "name": "Dianella",
-      "url":  "https://www.coles.com.au/find-stores/coles/wa/dianella-256"
-    },
-    {
       "name": "Mirrabooka",
       "url":  "https://www.coles.com.au/find-stores/coles/wa/mirrabooka-314"
     },
